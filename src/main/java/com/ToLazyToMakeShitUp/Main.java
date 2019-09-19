acb2372adde7432cd95d2b08102b9266da8b2032
Edgar <Edgar@AnotherFoxGuy.com>
2019-09-19 11:21:01 +0200

    :bug: Fixed

--- src/main/java/com/ToLazyToMakeShitUp/Main.java
+++ src/main/java/com/ToLazyToMakeShitUp/Main.java
@@ -6,32 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
         Hogeschool kaas = new Hogeschool("Kaas", "Boter", new ArrayList<Academie>() {{
-            new Academie("Technologie", "Technishe opleidingen", new ArrayList<School>() {{
-                new School("School of ICT", "Alle IT opleidingen");
+            add(new Academie("Technologie", "Technishe opleidingen", new ArrayList<School>() {{
+                add(new School("School of ICT", "Alle IT opleidingen"));
             }}, new ArrayList<Docent>() {{
-                new Docent("Jos", "Privé");
-                new Docent("Wouter", "Privé");
+                add(new Docent("Jos", "Privé"));
+                add(new Docent("Wouter", "Privé"));
             }}, new ArrayList<Opleiding>() {{
-                new Opleiding("Test", "Test", new ArrayList<Student>() {{
-                    new Student("Harry", "Amsterdam");
-                    new Student("Piet", "Limburg");
-                    new Student("Alex", "Groningen");
-                }});
-            }});
+                add(new Opleiding("Test", "Test", new ArrayList<Student>() {{
+                    add(new Student("Harry", "Amsterdam"));
+                    add(new Student("Piet", "Limburg"));
+                    add(new Student("Alex", "Groningen"));
+                }}));
+            }}));
 
-            new Academie("Zaklijk", "Zakelijke opleidingen", new ArrayList<School>() {{
-                new School("School of ICT", "Alle IT opleidingen");
+            add(new Academie("Zaklijk", "Zakelijke opleidingen", new ArrayList<School>() {{
+                add(new School("School of ICT", "Alle IT opleidingen"));
             }}, new ArrayList<Docent>() {{
-                new Docent("Jos", "Privé");
-                new Docent("Wouter", "Privé");
+                add(new Docent("Jos", "Privé"));
+                add(new Docent("Wouter", "Privé"));
             }}, new ArrayList<Opleiding>() {{
-                new Opleiding("Test", "Test", new ArrayList<Student>() {{
-                    new Student("Harry", "Amsterdam");
-                    new Student("Piet", "Limburg");
-                    new Student("Alex", "Groningen");
-                }});
-            }});
+                add(new Opleiding("Test", "Test", new ArrayList<Student>() {{
+                    add(new Student("Harry", "Amsterdam"));
+                    add(new Student("Piet", "Limburg"));
+                    add(new Student("Alex", "Groningen"));
+                }}));
+            }}));
         }});
+
+        System.out.println("Console is: ");
 
     }
 }
